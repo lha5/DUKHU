bd6fb2c1bfb1a6d12777db87532df94e6cb07d03
lha5 <hyeahnlee5@gmail.com>
2020-12-06 15:26:09 +0900

    클라이언트 라우팅 설정

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import NavBar from './views/NavBar/NavBar';
+import LandingPage from './views/LandingPage/LandingPage';
+import SignInPage from './views/SigninPage/SigninPage';
+import SignUpPage from './views/SignupPage/SignupPage';
+import Footer from './views/Footer/Footer';
+
 function App() {
   return (
-    <div className="App">
-      리액트 앱
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/signin" component={SignInPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+      </Switch>
+      <Footer />
+    </React.Fragment>
   );
 }
 
