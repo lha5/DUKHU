97f8f4f024215d50350f33cc9b23f9980d4c60cb
lha5 <hyeahnlee5@gmail.com>
2020-12-06 09:58:03 +0900

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+mongoose.connect('', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+}).then(() => console.log('MongoDB is connected...'))
+  .catch(error => console.error('MongoDB connecting ERROR:: ', error));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
